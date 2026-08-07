e5deccee10e8f2f66790177e8385189c01072824
maker-luder <hsuposhi1227@gmail.com>
2026-08-07 13:04:56 +0800

    continuity: export multidimensional evaluation API

--- components/continuity_governance_v0.1.0/src/aion_continuity_governance/__init__.py
+++ components/continuity_governance_v0.1.0/src/aion_continuity_governance/__init__.py
@@ -1,4 +1,27 @@
-from .models import ContinuityLayer, DriftDecision, DriftResult
-from .checks import check_interpretation_drift, continuity_status
+from .models import (
+    ContinuityDimension,
+    ContinuityLayer,
+    ContinuityMatrix,
+    DimensionObservation,
+    DriftDecision,
+    DriftResult,
+)
+from .checks import (
+    check_interpretation_drift,
+    continuity_matrix,
+    continuity_status,
+    correction_recovery_observation,
+)
 
-__all__ = ["ContinuityLayer", "DriftDecision", "DriftResult", "check_interpretation_drift", "continuity_status"]
+__all__ = [
+    "ContinuityDimension",
+    "ContinuityLayer",
+    "ContinuityMatrix",
+    "DimensionObservation",
+    "DriftDecision",
+    "DriftResult",
+    "check_interpretation_drift",
+    "continuity_matrix",
+    "continuity_status",
+    "correction_recovery_observation",
+]
