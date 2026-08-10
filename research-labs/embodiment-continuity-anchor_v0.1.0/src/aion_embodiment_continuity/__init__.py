977d0da0be2f0f0dc07bfd1f227ba0fabfd722c0
maker-luder <hsuposhi1227@gmail.com>
2026-08-10 09:26:59 +0800

    research: export multidimensional continuity assessment

--- research-labs/embodiment-continuity-anchor_v0.1.0/src/aion_embodiment_continuity/__init__.py
+++ research-labs/embodiment-continuity-anchor_v0.1.0/src/aion_embodiment_continuity/__init__.py
@@ -1,15 +1,21 @@
 from .models import (
     AnchorDecision,
+    ContinuityDimensionAssessment,
+    DimensionStatus,
     EmbodimentBinding,
     LineageAnchor,
     MigrationObservation,
     assess_anchor_continuity,
+    assess_continuity_dimensions,
 )
 
 __all__ = [
     "AnchorDecision",
+    "ContinuityDimensionAssessment",
+    "DimensionStatus",
     "EmbodimentBinding",
     "LineageAnchor",
     "MigrationObservation",
     "assess_anchor_continuity",
+    "assess_continuity_dimensions",
 ]
