446bf077e044d975051294218d3773baefdaff8f
maker-luder <hsuposhi1227@gmail.com>
2026-08-10 08:55:04 +0800

    research: add external evidence normalization package surface

---
+++ research-labs/external-evidence-normalization_v0.1.0/src/aion_external_evidence/__init__.py
@@ -0,0 +1,15 @@
+from .models import (
+    EvidenceDecision,
+    ExecutionMode,
+    ExternalEvidenceReport,
+    NormalizedEvidence,
+    normalize_external_report,
+)
+
+__all__ = [
+    "EvidenceDecision",
+    "ExecutionMode",
+    "ExternalEvidenceReport",
+    "NormalizedEvidence",
+    "normalize_external_report",
+]
