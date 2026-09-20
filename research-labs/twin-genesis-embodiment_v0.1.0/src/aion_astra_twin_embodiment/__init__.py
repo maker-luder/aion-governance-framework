b2a32b1c17235e0797ed815a882c997580f2f161
maker-luder <hsuposhi1227@gmail.com>
2026-09-21 00:40:01 +0800

    feat: export Teacher low-poly asset generator

--- research-labs/twin-genesis-embodiment_v0.1.0/src/aion_astra_twin_embodiment/__init__.py
+++ research-labs/twin-genesis-embodiment_v0.1.0/src/aion_astra_twin_embodiment/__init__.py
@@ -10,6 +10,7 @@ from .teacher_avatar import (
     build_teacher_avatar_gltf_contract,
     validate_teacher_avatar_contract,
 )
+from .teacher_avatar_asset import build_teacher_low_poly_gltf
 from .validation import ValidationError, validate_candidate
 
 __all__ = [
@@ -24,6 +25,7 @@ __all__ = [
     "TeacherBodyDimensions",
     "build_teacher_avatar_contract",
     "build_teacher_avatar_gltf_contract",
+    "build_teacher_low_poly_gltf",
     "validate_teacher_avatar_contract",
     "ValidationError",
     "validate_candidate",
