0d7d707782e7b44ca79a88ee0fbbc14fdcebc6ce
maker-luder <hsuposhi1227@gmail.com>
2026-09-21 00:43:37 +0800

    feat: export Teacher GLB reference generator

--- research-labs/twin-genesis-embodiment_v0.1.0/src/aion_astra_twin_embodiment/__init__.py
+++ research-labs/twin-genesis-embodiment_v0.1.0/src/aion_astra_twin_embodiment/__init__.py
@@ -10,7 +10,7 @@ from .teacher_avatar import (
     build_teacher_avatar_gltf_contract,
     validate_teacher_avatar_contract,
 )
-from .teacher_avatar_asset import build_teacher_low_poly_gltf
+from .teacher_avatar_asset import build_teacher_low_poly_glb, build_teacher_low_poly_gltf
 from .validation import ValidationError, validate_candidate
 
 __all__ = [
@@ -26,6 +26,7 @@ __all__ = [
     "build_teacher_avatar_contract",
     "build_teacher_avatar_gltf_contract",
     "build_teacher_low_poly_gltf",
+    "build_teacher_low_poly_glb",
     "validate_teacher_avatar_contract",
     "ValidationError",
     "validate_candidate",
