8fa915214c4d2c5c3dbe0b3870145c7ba48afad7
maker-luder <hsuposhi1227@gmail.com>
2026-09-21 01:34:42 +0800

    feat: export shared physiology reference API

--- research-labs/twin-genesis-embodiment_v0.1.0/src/aion_astra_twin_embodiment/__init__.py
+++ research-labs/twin-genesis-embodiment_v0.1.0/src/aion_astra_twin_embodiment/__init__.py
@@ -1,6 +1,14 @@
 """AION/Astra shared-genesis twin embodiment research candidate."""
 
 from .models import EmbodimentInstance, EmbodimentTemplate, SharedGenesisEvent
+from .physiology import (
+    REFERENCE_FUNCTIONAL_COMPLETENESS,
+    AdultMalePhysiologyReference,
+    PhysiologySystemReference,
+    build_adult_male_physiology_reference,
+    validate_adult_male_physiology_reference,
+    validate_physiology_parity,
+)
 from .runtime import TwinGenesisRuntime, TwinRuntimeState
 from .runtime_binding import TwinRuntimeContexts, build_runtime_contexts
 from .teacher_avatar import (
@@ -49,6 +57,12 @@ __all__ = [
     "EmbodimentInstance",
     "EmbodimentTemplate",
     "SharedGenesisEvent",
+    "PhysiologySystemReference",
+    "AdultMalePhysiologyReference",
+    "REFERENCE_FUNCTIONAL_COMPLETENESS",
+    "build_adult_male_physiology_reference",
+    "validate_adult_male_physiology_reference",
+    "validate_physiology_parity",
     "TwinGenesisRuntime",
     "TwinRuntimeState",
     "TwinRuntimeContexts",
